f92942eb816ca2246ea011d44f9aeefddb4d564d
FahmiRid <fahmi.ridwan9897@gmail.com>
2023-06-28 12:52:50 +0800

    Create home.jsx

---
+++ src/screen/home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import SideNav from "./SideNav";
+
+function Home() {
+  return (
+    <div>
+      <SideNav />
+      <div className="center">
+        <h1>home Page</h1>
+        <p>this is homepage testing</p>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
