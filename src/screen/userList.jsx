9cbaf74174bba4c048ef802bdd9237b7b5f4beba
FahmiRid <fahmi.ridwan9897@gmail.com>
2023-06-28 12:53:55 +0800

    Create userList.jsx

---
+++ src/screen/userList.jsx
@@ -0,0 +1,13 @@
+    import React from 'react'
+    import SideNav from './SideNav'
+    
+    function userList() {
+      return (
+        <div>
+            <SideNav />
+            <h1 className='center'> User List Page</h1>
+        </div>
+      )
+    }
+    
+    export default userList
