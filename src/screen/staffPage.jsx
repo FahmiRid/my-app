6abf63e734362f7dc10e90149129cdee10942c47
FahmiRid <fahmi.ridwan9897@gmail.com>
2023-06-28 12:53:48 +0800

    Create staffPage.jsx

---
+++ src/screen/staffPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SideNav from './SideNav'
+import '../styles/staffStyle.css';
+function staffPage() {
+  return (
+    <div>
+        <SideNav />
+        <h1 className='center'>Staff Dashboard Page</h1>
+    </div>
+  )
+}
+
+export default staffPage
