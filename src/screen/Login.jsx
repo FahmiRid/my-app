0bf3c9f51e7f20ac207d58c98638da8a26979bff
FahmiRid <95422790+FahmiRid@users.noreply.github.com>
2023-06-28 12:56:32 +0800

    Pt 1 (#1)
    * Create route path
    
    * adding logo image for login
    
    * Create home.jsx
    
    * create login page and css
    
    * Create permissionRole.jsx
    
    * Create SideNav.jsx
    
    * Create staffPage.jsx
    
    * Create userList.jsx
    
    * Create userRoles.jsx
    
    * Create homeStyle.css
    
    * adding CSS for page file
    
    * install the package libraries

---
+++ src/screen/Login.jsx
@@ -0,0 +1,94 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import '../styles/Login.css';
+import Images from '../images/logo.png'
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3001/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data.message); // Success message
+        console.log(data.user); // User object
+
+        // Redirect to the appropriate page based on user role
+        if (data.user.role === 'admin') {
+          navigate('/permissionRole');
+        } else if (data.user.role === 'staff') {
+          navigate('/staffPage');
+        } else {
+          // Handle other roles or redirect to a default page
+          navigate('/default-page');
+        }
+      } else {
+        const errorData = await response.json();
+        console.log(errorData.message); // Error message
+      }
+    } catch (error) {
+    //   console.error('Error:', error);
+      toast.error('Invalid username or password');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-card">
+        <div className="logo">
+          <img src={Images} alt=" Logo" />
+        </div>
+        <h1>Welcome back!</h1>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <div className="input-container">
+            <input
+              type="text"
+              placeholder="Username"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+          </div>
+          <div className="input-container">
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+        <div className="footer">
+          <span>Need an account?</span>
+          <a href="#">Register</a>
+        </div>
+        <ToastContainer />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/screen/permissionRole.jsx
@@ -0,0 +1,137 @@
+import React, { useState } from 'react';
+import '../styles/PermissionTable.css';
+import SideNav from './SideNav';
+const PermissionTable = () => {
+  const [permissions, setPermissions] = useState({
+    admin: {
+      create: false,
+      read: false,
+      edit: false,
+      delete: false,
+      activate: false,
+      reactivate: false,
+      deactivate: false,
+      approve: false,
+      
+    },
+    editor: {
+      create: false,
+      read: false,
+      edit: false,
+      delete: false,
+      activate: false,
+      reactivate: false,
+      deactivate: false,
+      approve: false,
+    },
+    viewer: {
+      create: false,
+      read: false,
+      edit: false,
+      delete: false,
+      activate: false,
+      reactivate: false,
+      deactivate: false,
+      approve: false,
+    },
+  });
+
+  // Handle checkbox change
+  const handleCheckboxChange = (role, permission) => {
+    setPermissions((prevPermissions) => ({
+      ...prevPermissions,
+      [role]: {
+        ...prevPermissions[role],
+        [permission]: !prevPermissions[role][permission],
+      },
+    }));
+  };
+
+  // Render the table view and checkboxes
+  return (
+    <div>
+        <SideNav />
+
+    <table className="permission-table">
+      <thead>
+        <tr>
+          <th>Role</th>
+          <th>Create</th>
+          <th>Read</th>
+          <th>Edit</th>     
+          <th>Delete</th>
+          <th>Activate</th>
+          <th>Reactivate</th>
+          <th>Deactivate</th>
+          <th>Approve</th>
+        </tr>
+      </thead>
+      <tbody>
+        {Object.keys(permissions).map((role) => (
+          <tr key={role}>
+            <td>{role}</td>
+            <td>
+              <input
+                type="checkbox"
+                checked={permissions[role].create}
+                onChange={() => handleCheckboxChange(role, 'create')}
+              />
+            </td>
+            <td>
+              <input
+                type="checkbox"
+                checked={permissions[role].read}
+                onChange={() => handleCheckboxChange(role, 'read')}
+              />
+            </td>
+            <td>
+              <input
+                type="checkbox"
+                checked={permissions[role].edit}
+                onChange={() => handleCheckboxChange(role, 'edit')}
+              />
+            </td>
+            <td>
+              <input
+                type="checkbox"
+                checked={permissions[role].delete}
+                onChange={() => handleCheckboxChange(role, 'delete')}
+              />
+            </td>
+            <td>
+              <input
+                type="checkbox"
+                checked={permissions[role].activate}
+                onChange={() => handleCheckboxChange(role, 'activate')}
+              />
+            </td>
+            <td>
+              <input
+                type="checkbox"
+                checked={permissions[role].reactivate}
+                onChange={() => handleCheckboxChange(role, 'reactivate')}
+              />
+            </td>
+            <td>
+              <input
+                type="checkbox"
+                checked={permissions[role].deactivate}
+                onChange={() => handleCheckboxChange(role, 'deactivate')}
+              />
+            </td>
+            <td>
+              <input
+                type="checkbox"
+                checked={permissions[role].approve}
+                onChange={() => handleCheckboxChange(role, 'approve')}
+              />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    </div>
+  );
+};
+
+export default PermissionTable;
