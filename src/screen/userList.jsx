d1109362a9752db30cf99578e660f6137bf93833
FahmiRid <95422790+FahmiRid@users.noreply.github.com>
2023-08-08 18:12:19 +0800

    adding button for modal popup (#7)

--- src/screen/userList.jsx
+++ src/screen/userList.jsx
@@ -5,12 +5,14 @@ import UserTable from "./userTable";
 
 function UserList() {
   const username = ('fahmi' || 'ridwan');
+
   return (
     <div>
       <SideNav username={username} />
       <div className="center">
         <h1 style={{ color: "black" }}>User List</h1>
         <p>This is the user list page.</p>
+        <button className="btnAdd" onClick={() => console.log("Add roles clicked")}>Add roles</button>
         <UserTable />
       </div>
     </div>
