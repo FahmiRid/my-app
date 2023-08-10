e69c665cfe2d549ce5100cf42442f93946cdf55d
FahmiRid <95422790+FahmiRid@users.noreply.github.com>
2023-08-11 01:30:36 +0800

    Adding Pagination for user list (#8)

--- src/screen/userTable.jsx
+++ src/screen/userTable.jsx
@@ -3,6 +3,9 @@ import "../styles/userTableStyles.css";
 
 function UserTable() {
   const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const usersPerPage = 5; // You can adjust this number as needed
+
 
   useEffect(() => {
     fetchUsers();
@@ -18,27 +21,47 @@ function UserTable() {
     }
   }
 
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Username</th>
-          <th>Role</th>
-          <th>Action</th> {/* New column for actions */}
-        </tr>
-      </thead>
-      <tbody>
-        {users.map((user, index) => (
-          <tr key={index}>
-            <td>{user.username}</td>
-            <td>{user.role}</td>
-            <td>
-              <a className="editAction">Edit</a>
-            </td>
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>Role</th>
+            <th>Action</th> {/* New column for actions */}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {users
+            .slice((currentPage - 1) * usersPerPage, currentPage * usersPerPage)
+            .map((user, index) => (
+              <tr key={index}>
+                <td>{user.username}</td>
+                <td>{user.role}</td>
+                <td>
+                  <a className="editAction">Edit</a>
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+      <div className="pagination">
+        <button
+          disabled={currentPage === 1}
+          onClick={() => setCurrentPage(currentPage - 1)}
+        >
+          Previous
+        </button>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={currentPage === Math.ceil(users.length / usersPerPage)}
+          onClick={() => setCurrentPage(currentPage + 1)}
+        >
+          Next
+        </button>
+      </div>
+    </div>
   );
 }
 
