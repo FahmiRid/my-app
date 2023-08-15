ad41228ca1393ea4138c1212f556e38557cea554
FahmiRid <fahmi.ridwan9897@gmail.com>
2023-08-15 17:07:31 +0800

    update addEventListener

--- src/screen/Admin.jsx
+++ src/screen/Admin.jsx
@@ -1,41 +1,32 @@
-import {React,  useEffect } from "react";
+import { React, useEffect, } from "react";
 import "../styles/admin1.css";
 import "semantic-ui-css/semantic.min.css";
-import { Button, Card, Image } from "semantic-ui-react";
+import { Button, Card } from "semantic-ui-react";
+import { useNavigate } from "react-router-dom";
 
 export default function Admin() {
+  const navigate = useNavigate();
   const sendMessagetoReactNative = () => {
     const message = "password_buy";
     window.ReactNativeWebView && window.ReactNativeWebView.postMessage(message);
     console.log("received", message);
   };
 
-  
-
-  // useEffect(() => {
-  //   const retrieveMessage = () => {
-  //     document.addEventListener("message", function (event) {
-  //       setTimeout(function(){document.postMessage("WebView message")});
-  //       console.log('retrieve', event)
-  //     }, false);
-  //     console.log('retrieveMessage')
-  //   }
-  // }, []);
-
   useEffect(() => {
     const handleMessageFromWebView = event => {
-      // Handle the received message from the WebView
       // console.log("Received message from WebView:", event);
-      alert("Received message:" + JSON.stringify(event.data));
+      if (event.data === "password_buy_AA") {
+        alert("Received message:" + JSON.stringify(event.data));
+        navigate("/admin2");
+      }
     };
 
     window.addEventListener("message", handleMessageFromWebView);
 
-    return () => {
-      window.removeEventListener("message", handleMessageFromWebView);
-    };
-  }, []);
-
+    // return () => {
+    //   window.removeEventListener("message", handleMessageFromWebView);
+    // };
+  }, [navigate]);
 
   return (
     <div className="center">
