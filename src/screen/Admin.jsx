fea70b6ae359fc9af09879caf1ec1a71daf3e487
FahmiRid <fahmi.ridwan9897@gmail.com>
2023-08-15 12:08:49 +0800

    added semantic UI

--- src/screen/Admin.jsx
+++ src/screen/Admin.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import '../styles/admin1.css'
+import 'semantic-ui-css/semantic.min.css'
+import { Button, Card, Image } from 'semantic-ui-react'
 
 export default function Admin() {
   useEffect(() => {
@@ -34,15 +36,48 @@ export default function Admin() {
 
   return (
     <div className="center">
-    <div className="border-box">
+    {/* <div className="border-box">
       <h1>First Page</h1>
-      <button
+      <Button
         onClick={sendMessagetoReactNative}
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+       
       >
-        Buy Sell
-      </button>
-    </div>
+        Buy 
+      </Button>
+      <Button
+        onClick={sendMessagetoReactNative}
+      >
+        Sell
+      </Button>
+    </div> */}
+
+<Card.Group>
+    <Card>
+      <Card.Content>
+        {/* <Image
+          floated='right'
+          size='mini'
+          src='/images/avatar/large/steve.jpg'
+        /> */}
+        <Card.Header>Foreign Currency</Card.Header>
+        <Card.Meta>Total Invesment</Card.Meta>
+        <Card.Description>
+           <strong>RM45,000,00</strong>
+        </Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        <div className='ui two buttons'>
+          <Button basic color='green' onClick={sendMessagetoReactNative}>
+            Buy
+          </Button>
+          <Button basic color='red' onClick={sendMessagetoReactNative}>
+            Sell
+          </Button>
+        </div>
+      </Card.Content>
+    </Card>
+  </Card.Group>
+
     </div>
   );
 }
