de594cb04114a00884d932a28a26d3f42a7aa29b
FahmiRid <fahmi.ridwan9897@gmail.com>
2023-08-15 10:30:43 +0800

    deploy for message

--- src/screen/Admin.jsx
+++ src/screen/Admin.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 export default function admin() {
+
+  const sendMessagetoReactNative = () => {
+    window.ReactNativeWebView.postMessage("password_buy!");
+    console.log("meessage ==> ", sendMessagetoReactNative);
+  }
   return (
     <div className="flex justify-center items-center h-screen">
         <h1>First Page</h1>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <button onClick={sendMessagetoReactNative} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Buy Sell
       </button>
     </div>
