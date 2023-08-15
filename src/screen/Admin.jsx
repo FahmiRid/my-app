21c4c2b5abebb0358224eef633c904baa55d9f75
FahmiRid <fahmi.ridwan9897@gmail.com>
2023-08-15 14:38:08 +0800

    update the data message

--- src/screen/Admin.jsx
+++ src/screen/Admin.jsx
@@ -1,60 +1,38 @@
 import React, { useEffect } from "react";
-import '../styles/admin1.css'
-import 'semantic-ui-css/semantic.min.css'
-import { Button, Card, Image } from 'semantic-ui-react'
+import "../styles/admin1.css";
+import "semantic-ui-css/semantic.min.css";
+import { Button, Card, Image } from "semantic-ui-react";
 
 export default function Admin() {
-
   const sendMessagetoReactNative = () => {
-    const message = "password_buy!";
+    const message = "password_buy";
     window.ReactNativeWebView && window.ReactNativeWebView.postMessage(message);
-    console.log ("received!",message);
-  }
+    console.log("received", message);
+  };
 
   return (
     <div className="center">
-    {/* <div className="border-box">
-      <h1>First Page</h1>
-      <Button
-        onClick={sendMessagetoReactNative}
-       
-      >
-        Buy 
-      </Button>
-      <Button
-        onClick={sendMessagetoReactNative}
-      >
-        Sell
-      </Button>
-    </div> */}
-
-<Card.Group>
-    <Card>
-      <Card.Content>
-        {/* <Image
-          floated='right'
-          size='mini'
-          src='/images/avatar/large/steve.jpg'
-        /> */}
-        <Card.Header>Foreign Currency</Card.Header>
-        <Card.Meta>Total Invesment</Card.Meta>
-        <Card.Description>
-           <strong>RM45,000,00</strong>
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <div className='ui two buttons'>
-          <Button basic color='green' onClick={sendMessagetoReactNative}>
-            Buy
-          </Button>
-          <Button basic color='red' onClick={sendMessagetoReactNative}>
-            Sell
-          </Button>
-        </div>
-      </Card.Content>
-    </Card>
-  </Card.Group>
-
+      <Card.Group>
+        <Card>
+          <Card.Content>
+            <Card.Header>Foreign Currency</Card.Header>
+            <Card.Meta>Total Invesment</Card.Meta>
+            <Card.Description>
+              <strong>RM45,000,00</strong>
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <div className="ui two buttons">
+              <Button basic color="green" onClick={sendMessagetoReactNative}>
+                Buy
+              </Button>
+              <Button basic color="red" onClick={sendMessagetoReactNative}>
+                Sell
+              </Button>
+            </div>
+          </Card.Content>
+        </Card>
+      </Card.Group>
     </div>
   );
 }
