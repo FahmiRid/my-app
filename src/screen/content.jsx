ba022d0e5aa08afead61eeac45e6592238a855e1
FahmiRid <95422790+FahmiRid@users.noreply.github.com>
2023-10-01 04:30:19 +0800

    Update content.jsx

--- src/screen/content.jsx
+++ src/screen/content.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import SideNav from "./SideNav";
+import SideNav2 from "./SideNav2";
 import "../styles/contentStyles.css";
 
 function content() {
   const username = "fahmi";
   return (
     <div>
-      <SideNav username={username} />
+      <SideNav2  />
       <div className="center-font"> 
         <h2>Content</h2>
       </div>    
